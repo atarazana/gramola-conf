cb409da78a8e3efc8fff36e9e654182392832483
cvicens <carlos.vicens.alonso@gmail.com>
2022-11-17 18:15:04 +0100

    from gramola

--- events-deployment/overlays/dev/run.sh
+++ events-deployment/overlays/dev/run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 ARGOCD_APP_NAME=events
-#DEBUG="true"
-DESTINATION_NAME=in-cluster
-GIT_URL=https://github.com
-GIT_USERNAME=atarazana
-BASE_REPO_NAME=gramola-conf
-GIT_REVISION=main
-helm template ../../helm_base --name-template $ARGOCD_APP_NAME \
-  --set debug=${DEBUG},clusterName=${DESTINATION_NAME},gitUrl=${GIT_URL},gitUsername=${GIT_USERNAME},baseRepoName=${BASE_REPO_NAME},gitRevision=${GIT_REVISION} \
-  --include-crds > ../../helm_base/all.yml && kustomize build
+BASE_REPO_URL=https://github.com/atarazana/gramola
+NAMESPACE_SUFFIX="-user1"
+helm template ../../helm_base --name-template $ARGOCD_APP_NAME --set debug=${DEBUG},baseRepoUrl=${BASE_REPO_URL} --include-crds > ../../helm_base/all.yml && kustomize build | sed "s/\(namespace:[[:space:]]\{1,\}\)\(gramola-.*\)/\1\2${NAMESPACE_SUFFIX}/"
--- events-deployment/overlays/test/run.sh
+++ events-deployment/overlays/test/run.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 ARGOCD_APP_NAME=gramola-events
-DESTINATION_NAME=in-cluster
-GIT_URL=https://github.com
-GIT_USERNAME=atarazana
-BASE_REPO_NAME=gramola-conf
-GIT_REVISION=main
-helm template ../../helm_base --name-template $ARGOCD_APP_NAME \
-  --set debug=${DEBUG},clusterName=${DESTINATION_NAME},gitUrl=${GIT_URL},gitUsername=${GIT_USERNAME},baseRepoName=${BASE_REPO_NAME},gitRevision=${GIT_REVISION} \
-  --include-crds > ../../helm_base/all.yml && kustomize build
+helm template ../../helm_base --name-template ${ARGOCD_APP_NAME} --include-crds > ../../helm_base/all.yml && kustomize build
